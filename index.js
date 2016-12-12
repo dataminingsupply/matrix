0fb142b7fc04f5be7bfdb9cbe3ec239178c489ee
Kapil Garg <kapil1garg@gmail.com>
2016-12-12 15:34:24 -0600

    adding routes for matrix ops

--- index.js
+++ index.js
@@ -1,55 +1,85 @@
-/*
- *  All supplies must modules.export an object
- */
+var math = require('./math.js');
+
 module.exports = {
-
-  /*
-   *  $inject is an optional function for initializing persistent variables
-   *  and using other supplies within this supply.
-   */
   $inject: function(supply, config){
-    /*
-     * supply:    an instance of the @dms/supply package, used for loading nested supplies.
-     *            If a supply is loaded here, return it as an attribute to make 
-     *            it available in $main and any routes.
-     *
-     * config:    object containing parameter settings
-     *
-     */
-
     return;
   },
 
-  /*
-   *  $main is the required function that will be called via a supply's default route.
-   *  The core of your supply logic should go here.
-   */
   $main: function($, data, config, callback){
-    /*
-     *  $:        an internal stateless storage object and holds any miners and
-     *            injected attributes from the $inject() function
-     *
-     *  data:     the incoming data as a JSON object
-     *
-     *  config:   an object containing parameter settings
-     *
-     *  callback: a function for completing the processing of this data point.
-     *            Use the first parameter for errors and the second as an object
-     *            for output data - callback(err, outputs)
-     */
-
     return callback(null, data);
   },
 
-  /*
-   *  $on is an optional object that creates alternate routes for this supply.
-   *  This is often useful for data coming from different sources or for different purposes.
-   */
   $on: {
-    route_name: function($, data, config, callback){
-      // TODO: write endpoint logic
+    size: function($, data, config, callback) {
+      try {
+        var output = {
+          'output': math.size(data.matrixA)
+       };
+        return callback(null, output);
+      } catch (err) {
+        return callback(err.toString(), null);
+      }
 
-      return callback(null, data);
+    },
+    add: function($, data, config, callback) {
+      try {
+        var output = {
+          'output': math.add(data.matrixA, data.matrixB)
+        };
+        return callback(null, output);
+      } catch (err) {
+        return callback(err.toString(), null);
+      }
+    },
+    subtract: function($, data, config, callback) {
+      try {
+        var output = {
+          'output': math.subtract(data.matrixA, data.matrixB)
+        };
+        return callback(null, output);
+      } catch (err) {
+        return callback(err.toString(), null);
+      }
+    },
+    multiply: function($, data, config, callback) {
+      try {
+        var output = {
+          'output': math.multiply(data.matrixA, data.matrixB)
+        };
+        return callback(null, output);
+      } catch (err) {
+        return callback(err.toString(), null);
+      }
+    },
+    transpose: function($, data, config, callback) {
+      try {
+        var output = {
+          'output': math.transpose(data.matrixA)
+        };
+        return callback(null, output);
+      } catch (err) {
+        return callback(err.toString(), null);
+      }
+    },
+    determinant: function($, data, config, callback) {
+      try {
+        var output = {
+          'output': math.det(data.matrixA)
+        };
+        return callback(null, output);
+      } catch (err) {
+        return callback(err.toString(), null);
+      }
+    },
+    inverse: function($, data, config, callback) {
+      try {
+        var output = {
+          'output': math.inv(data.matrixA)
+        };
+        return callback(null, output);
+      } catch (err) {
+        return callback(err.toString(), null);
+      }
     }
   }
 };
