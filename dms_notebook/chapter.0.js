579a9dc8b3501c5cfc7da13248b7daf5f9bdb8ee
Kapil Garg <kapil1garg@gmail.com>
2016-12-12 14:15:08 -0600

    inital supply setup

---
+++ dms_notebook/chapter.0.js
@@ -0,0 +1,15 @@
+var supply = require('@dms/io');
+supply().setRelativeMode(true);
+var dms = supply().notebook;
+
+/*
+ * Hi there!
+ *
+ * You can ignore everything above this comment. We set some stuff up
+ * automatically so that it's always easy to get started using Data Markdown.
+ * More info and resources can be found on our site: https://dm.supply
+ *
+ * - The DMS Team
+ *
+ * Data Markdown v0.6.1
+ */
